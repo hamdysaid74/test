a2bf6727462636417bae8c8b80b6638bbcfb25fb
hamdysaid74 <109464149+hamdysaid74@users.noreply.github.com>
2022-09-28 21:29:18 +0200

    Add files via upload

---
+++ anode.c
@@ -0,0 +1,11 @@
+/* assignment 1 the number if it anode */
+zero = 0Xco ;
+one = 0xfd;
+two = 0xA4 ;
+three = 0xB0 ;
+four = 0x90 ;
+five = 0x99 ; 
+six = 0x82 ;
+seven = 0xf8 ; 
+eight = 0x80 ; 
+nine = 0xb0 ; 
